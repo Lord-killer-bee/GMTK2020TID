be1d7c67663893c78948ec353b0bd528a3d6866e
Trennig <basheermujavar@gmail.com>
2020-07-11 04:01:38 +0530

    Level manager(WIP)

---
+++ ChaoticShooter/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private float rowCount;
+    [SerializeField] private float columnCount;
+
+    public static float minMarginX = -5;
+    public static float maxMarginX = 5;
+    public static float minMarginZ = -5;
+    public static float maxMarginZ = 5;
+
+    void Start()
+    {
+        SetupLevel();
+    }
+
+    public void SetupLevel()
+    {
+        minMarginX = -rowCount / 2;
+        maxMarginX = -minMarginX;
+        minMarginZ = -columnCount / 2;
+        maxMarginZ = -minMarginZ;
+
+
+    }
+}
--- ChaoticShooter/Assets/Scripts/Player/PlayerController.cs
+++ ChaoticShooter/Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,7 @@ public class PlayerController : MonoBehaviour
 
     Vector3 moveDirection = Vector3.zero;
     Vector3 targetPosition = Vector3.zero;
-    float minX = -5;
-    float maxX = 5;
-    float minZ = -5;
-    float maxZ = 5;
 
-    // Update is called once per frame
     void Update()
     {
         moveDirection.x = Input.GetAxisRaw("Horizontal"); 
@@ -23,16 +18,16 @@ public class PlayerController : MonoBehaviour
 
         transform.LookAt(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
 
-        if (targetPosition.x + transform.position.x < minX)
+        if (targetPosition.x + transform.position.x < LevelManager.minMarginX)
             targetPosition.x = 0;
 
-        if (targetPosition.x + transform.position.x > maxX)
+        if (targetPosition.x + transform.position.x > LevelManager.maxMarginX)
             targetPosition.x = 0;
 
-        if (targetPosition.z + transform.position.z < minZ)
+        if (targetPosition.z + transform.position.z < LevelManager.minMarginZ)
             targetPosition.z = 0;
 
-        if (targetPosition.z + transform.position.z > maxZ)
+        if (targetPosition.z + transform.position.z > LevelManager.maxMarginZ)
             targetPosition.z = 0;
 
         transform.Translate(targetPosition, Space.World);
